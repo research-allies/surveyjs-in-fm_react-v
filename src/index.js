27b033cef2a5a08ba3a007ca134f53df79195b65
integrating-magic <jeremy@integratingmagic.io>
2024-04-23 10:26:36 -0400

    final changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import MyApp from "./myApp";
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<MyApp />);
+window.loadApp = (json) => {
+  const obj = JSON.parse(json);
+  console.log(obj);
+  const container = document.getElementById("root");
+  const root = createRoot(container);
+  root.render(<MyApp obj={obj} />);
+};
--- src/myApp.js
+++ src/myApp.js
@@ -1,33 +1,58 @@
-import { useCallback } from "react";
-
-import "survey-core/defaultV2.min.css";
+import React, { useState, useCallback } from "react";
+import { DefaultLight } from "survey-core/themes/default-light.js";
+import { BorderlessLightPanelless } from "survey-core/themes/borderless-light-panelless..js";
+import { BorderlessLight } from "survey-core/themes/borderless-light.js";
 import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
-const MyApp = () => {
-  const surveyJson = {
-    elements: [
-      {
-        name: "FirstName",
-        title: "Enter your first name:",
-        type: "text",
-      },
-      {
-        name: "LastName",
-        title: "Enter your last name:",
-        type: "text",
-      },
-    ],
+import "survey-core/i18n/thai";
+let survey;
+window.loadResponse = (json) => {
+  const obj = JSON.parse(json);
+  const { response = {} } = obj;
+  console.log(response);
+  survey.data = response;
+};
+
+window.saveIncompleteSurvey = () => {
+  const data = survey.data;
+  FileMaker.PerformScriptWithOption(
+    "Callback_Retrieve Incomplete Survey",
+    JSON.stringify(data),
+    5
+  );
+};
+const MyApp = ({ obj }) => {
+  const {
+    showTimer = "none",
+    showProgressBar = "off",
+    surveyJSON = {},
+    response = {},
+    mode = "edit",
+    callback,
+    panels = 1,
+    locale = "en",
+  } = obj;
+  window.displayMode = (mode) => {
+    console.log(survey.data);
+    setDisplayMode(mode);
   };
-  const survey = new Model(surveyJson);
-  survey.focusFirstQuestionAutomatic = false;
+  survey = new Model(surveyJSON);
+  survey.data = {
+    FirstName: "Jeremy",
+  };
+  survey.focusFirstQuestionAutomatic = true;
 
-  const alertResults = useCallback((sender) => {
+  const handleSave = useCallback((sender) => {
     const results = JSON.stringify(sender.data);
-    alert(results);
+    FileMaker.PerformScript(callback, results);
   }, []);
-
-  survey.onComplete.add(alertResults);
-
+  survey.data = response;
+  survey.onComplete.add(handleSave);
+  survey.applyTheme(DefaultLight);
+  survey.showProgressBar = showProgressBar;
+  survey.showTimerPanel = showTimer;
+  survey.mode = mode;
+  survey.locale = locale;
   return <Survey model={survey} />;
 };
 
